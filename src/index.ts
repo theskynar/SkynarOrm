85ea7323422bee4926a173fbb4d29baadf917ca0
Enrico Alvarenga <enricomalvarenga@gmail.com>
2017-10-04 13:40:00 -0300

    Fixing export of packages

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import * as SkynarValidator from "class-validator";
 import * as SkynarOrmInjection from "typeorm-typedi-extensions";
 import * as SkynarConnectionFactory from "./Connection/index";
 
-export default {
+export {
     SkynarOrm,
     SkynarValidator,
     SkynarOrmInjection,
